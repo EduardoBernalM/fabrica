f0d59bcb32ab8395510e40a8bb711363d98877b5
EduardoBernalM <bernalmoralesluiseduardo@gmail>
2022-10-12 16:08:46 -0500

    fabrica

---
+++ fabrica.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE FABRICA; 
+USE FABRICA; 
+CREATE TABLE fabricante(
+  codigo INT(10),
+  nombre VARCHAR(100),
+  PRIMARY KEY (codigo)
+); 
+CREATE TABLE producto( 
+  codigo INT(10), 
+  nombre VARCHAR(100),
+  precio DOUBLE, 
+  codigo_fabricante INT(10),
+  FOREIGN KEY (codigo_fabricante) REFERENCES fabricante(codigo)
+); 
+  INSERT INTO fabricante(codigo,nombre)VALUES (1,"Asus"),(2,"Lenovo"),(3,"Hewlett-Packard"),(4,"Samsumg"),(5,"Seagate"),(6,"Crucial"),(7," Gigabyte"),(8,"Huawei"),(9,"Xiaomi");
+  INSERT INTO producto(codigo,nombre,precio,codigo_fabricante) VALUES (1,"Disco duro SATA3 1TB",86.99,5),(2,"Memoria RAM DDR4 8GB",120,6),(3,"Disco SSD 1TB",150.99,4),(4,"GeForce GTX 1050 Ti",185,7),(5,"GeForce GTX 1080 Xtreme",755,6),(6,"Monitor 24 LED Full HD",202,1),(7,"Monitor 27 LED Full HD",245.99,1),(8,"Portatil yoga 520",559,2),(9,"Portatil Ideapad 320",444,2), (10,"Impresora HP Deskjet 3720",59.99,3),(11,"Impresora HP LaserJet Pro M26nw",180,3);
